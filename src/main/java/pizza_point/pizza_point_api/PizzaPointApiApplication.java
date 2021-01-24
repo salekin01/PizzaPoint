4a0d101280e1070ff3bca47273ada155bc60740e
salekin01 <salekin01@outlook.com>
2021-01-24 22:14:34 +0100

    initial project commit

---
+++ src/main/java/pizza_point/pizza_point_api/PizzaPointApiApplication.java
@@ -0,0 +1,13 @@
+package pizza_point.pizza_point_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPointApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaPointApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pizza_point/pizza_point_api/PizzaPointApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pizza_point.pizza_point_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaPointApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
